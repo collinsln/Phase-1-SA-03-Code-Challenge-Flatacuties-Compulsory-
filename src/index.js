0e338c0cb9b872931a69e9a361db080d5fe758ac
collinsln <collangat@gmail.com>
2023-10-17 21:49:51 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-// Your code here
+const fetchCharacterData = async () => {
+    const response = await fetch('db.json');
+    const data = await response.json();
+    return data.characters;
+  };
+ 
+  const characterBar = document.querySelector('#character-bar');
+
+async function renderCharacterNames() {
+  const characters = await fetchCharacterData();
+
+  characters.forEach((character) => {
+    const span = document.createElement('span');
+    span.textContent = character.name;
+    characterBar.appendChild(span);
+  });
+}
+
+renderCharacterNames();
